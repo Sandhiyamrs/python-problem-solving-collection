0c7e7cb8a05dfa537688bd4c9c61a494f622da6f
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-27 21:04:46 +0530

    Create solution_optimized.py

---
+++ 18_random_password_generator/solution_optimized.py
@@ -0,0 +1,10 @@
+import cv2
+
+class ImageSketcher:
+    def convert(self, input_path, output_path="sketch.png"):
+        image = cv2.imread(input_path)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        inverted = cv2.bitwise_not(gray)
+        blurred = cv2.GaussianBlur(inverted, (21, 21), 0)
+        sketch = cv2.divide(gray, 255 - blurred, scale=256)
+        cv2.imwrite(output_path, sketch)
