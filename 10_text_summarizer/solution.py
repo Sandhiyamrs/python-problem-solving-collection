6fc97368b4317448a3b382b2c8bc1a863c96ceb0
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-27 20:49:22 +0530

    Update solution.py

--- 10_text_summarizer/solution.py
+++ 10_text_summarizer/solution.py
@@ -1,16 +1,3 @@
-import re
-from collections import Counter
-
-def summarize(text: str, max_sentences=3) -> str:
-    sentences = re.split(r'(?<=[.!?]) +', text)
-    words = re.findall(r'\w+', text.lower())
-
-    freq = Counter(words)
-    sentence_scores = {}
-
-    for sentence in sentences:
-        score = sum(freq.get(word.lower(), 0) for word in re.findall(r'\w+', sentence))
-        sentence_scores[sentence] = score
-
-    summary = sorted(sentence_scores, key=sentence_scores.get, reverse=True)
-    return " ".join(summary[:max_sentences])
+def summarize(text, sentence_count=2):
+    sentences = text.split(".")
+    return ".".join(sentences[:sentence_count]) + "."
